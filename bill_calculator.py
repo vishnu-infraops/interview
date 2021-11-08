329eae50cd6822d5a99bc11a424d8124193c57e7
vishnu-infraops <83286661+vishnu-infraops@users.noreply.github.com>
2021-11-08 09:50:07 +0530

    Comments added

--- bill_calculator.py
+++ bill_calculator.py
@@ -5,18 +5,18 @@ charge = 0
 
 for index,slab in enumerate(slabs_charges):
 
-    if index == 0:
-       if your_unit < slabs_charges[index][1]:
+    if index == 0: ## Start of  slab_charges
+       if your_unit < slabs_charges[index][1]: ## Less than minimum slab ,exiting after the calculation
           charge = your_unit*slabs_charges[index][0]
           break
        else:
           charge = slabs_charges[index][0]*slabs_charges[index][1]
-    elif your_unit <= slabs_charges[index][1]:
+    elif your_unit <= slabs_charges[index][1]:  ## Less or equal to from the Index slab charges,exiting after the calculation
        charge = charge + (your_unit - slabs_charges[index-1][1])*slabs_charges[index][0]
        break
     else:
        charge = charge + slabs_charges[index][0]*(slabs_charges[index][1]-slabs_charges[index-1][1])
-       if index+1 == len(slabs_charges):
+       if index+1 == len(slabs_charges):  ## Out of slabs_charges List ,rest of unit will get multiplied here
           charge = charge + (your_unit - slabs_charges[index][1])*slabs_charges[index][0]
 
 print charge
